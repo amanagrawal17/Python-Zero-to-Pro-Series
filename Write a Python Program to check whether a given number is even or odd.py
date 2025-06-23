d2669b3fe11dd8a6577b4698a5996986016e0c54
Aman <amanagrawal1782@gmail.com>
2025-06-23 11:19:10 +0530

    Program 1 of python zero to pro

---
+++ Write a Python Program to check whether a given number is even or odd.py
@@ -0,0 +1,11 @@
+'''Write a Python Program to check whether a given number is even or odd.'''
+
+def check(n):
+    if n % 2 == 0:
+        print("The number", n , "is even. ")
+    else :
+        print("The number", n , "is odd.")   
+        
+
+
+check(10)
